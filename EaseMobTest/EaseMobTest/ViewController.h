7ae9dd97c2e74e61bd0b9c44655cd13866478cb9
hxx0215 <hxx0215@126.com>
2016-02-27 20:22:58 +0800

    init

---
+++ EaseMobTest/EaseMobTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EaseMobTest
+//
+//  Created by hxx on 16/2/27.
+//  Copyright © 2016年 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
